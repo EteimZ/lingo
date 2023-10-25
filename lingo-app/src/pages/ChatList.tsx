311249e78adf4bc8914c426167da843862e42bfe
Ebiladou <oyinebiladouomemu@gmail.com>
2023-10-25 19:45:54 +0100

    added page merge

--- lingo-app/src/pages/ChatList.tsx
+++ lingo-app/src/pages/ChatList.tsx
@@ -1,6 +1,6 @@
-import { Avatar, AvatarBadge, Center, HStack, Heading } from "@chakra-ui/react";
+import { Avatar, AvatarBadge, Center, HStack, Heading, Box, Grid, GridItem, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import ChatDetail from "./ChatDetail";
 
 import socket from "../socket";
 
@@ -11,7 +11,6 @@ interface User {
 }
 
 function ChatList() {
-  const navigate = useNavigate();
 
   const [users, setUsers] = useState<User[]>([]);
 
@@ -57,31 +56,41 @@ function ChatList() {
 
   return (
     <>
-      <Center>
-        <Heading color="green.400">List of chats</Heading>
-      </Center>
-      {users.map(({ username, userID, connected }, index) => (
-        <HStack
-          boxShadow="base"
-          borderColor="green.200"
-          borderRadius={4}
-          cursor="pointer"
-          p={2}
-          mx={2}
-          my={2}
-          key={index}
-          onClick={() => navigate(`/detail/${username}/${userID}`)}
-        >
-          <Avatar
+      <Box h={"100vh"} bg={"black.400"}>
+      <Grid templateColumns={"25% 75%"} bg={"green.500"}>
+      <GridItem bg={"teal.800"}>
+        <Center>
+          <Heading color="green.400">List of chats</Heading>
+        </Center>
+        {users.map(({ username, connected }, index) => (
+          <HStack
+            boxShadow="base"
+            borderColor="green.200"
+            borderRadius={4}
+            cursor="pointer"
+            p={2}
+            mx={2}
+            my={2}
+            key={index}
+          >
+            <Avatar
             name={username}
             src="https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg"
           >
             { connected ? <AvatarBadge boxSize="1.25em" bg="green.500" /> :
             <AvatarBadge boxSize="1.25em" bg="red.500" /> }
-          </Avatar>
-          <p>{username}</p>
-        </HStack>
-      ))}
+          </Avatar>        
+            <Text color={"yellow.500"}>{username}</Text>
+          </HStack>
+        ))}
+        </GridItem>
+  
+        <GridItem bg={"teal.600"}>
+        <ChatDetail/>
+        </GridItem>
+
+        </Grid>
+        </Box>
     </>
   );
 }
